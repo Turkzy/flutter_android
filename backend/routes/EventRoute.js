88866413093ffc8ec8c471096dbd45343f8c13cc
Turkzy <165537971+Turkzy@users.noreply.github.com>
2024-06-18 23:24:05 +0800

    123

--- backend/routes/EventRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getEvents } from "../controllers/EventController.js";
-
-const router = express.Router();
-
-router.get('/events', getEvents);
-
-export default router;
