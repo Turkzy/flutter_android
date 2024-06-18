5a8a005351f2b3babbdea85de544a4adcd83f244
Alphanimos <165538455+Alphanimos@users.noreply.github.com>
2024-06-18 23:23:02 +0800

    111

--- backend/routes/CourseRoute.js
+++
@@ -1,9 +0,0 @@
-// routes/CourseRoute.js
-import express from "express";
-import { getCourses} from "../controllers/CourseController.js";
-
-const router = express.Router();
-
-router.get('/courses', getCourses);
-
-export default router;
--- backend/routes/ScheduleRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getSchedulesByUserId } from "../controllers/ScheduleController.js";
-
-const router = express.Router();
-
-router.get("/schedules/:user_id", getSchedulesByUserId);
-
-export default router;
